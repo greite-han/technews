2cb61e9ad69639e5fc2c61578410cf0ae2954a46
Minsung Han <minsunghan@Minsungs-MacBook-Pro.local>
2025-05-04 15:19:58 +0900

    Revert to simple Firebase Hosting

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const app = express();
-
-app.use(express.static('public'));
-
-// 모든 경로에 대해 index.html을 서빙
-app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-exports.app = functions.https.onRequest(app); 
+// Firebase Hosting은 정적 파일만 서빙하므로 이 파일은 필요하지 않습니다.
+// public/index.html이 직접 서빙됩니다. 
